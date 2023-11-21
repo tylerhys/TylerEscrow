9aa192c5c3ae9a0a03a438dcab8dd302c77cd8a1
tylerhys <73792788+tylerhys@users.noreply.github.com>
2023-11-21 09:51:22 +0800

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { addEscrow, setEscrows, updateEscrowApprovalStatus } from './escrowSlice'; // Import your Redux actions
+import { addEscrow, setEscrows, updateEscrowApprovalStatus } from './escrowSlice'; // Import your Redux actionss
 import deploy from './deploy';
 import Escrow from './Escrow';
 import { ethers } from 'ethers';
